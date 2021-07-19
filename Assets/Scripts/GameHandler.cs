5bf407cbb43e69a8182fc7ebb48653421e1c1ed4
sh960440 <54958110+sh960440@users.noreply.github.com>
2021-07-19 03:05:02 -0400

    Check Lose Conditions

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -7,14 +7,17 @@ public class GameHandler : MonoBehaviour
     private CardStack cardStack;
     private GameUIController gameUI;
     private List<Player> players;
+    private bool[] didPlayersSlap;
     private int currentPlayer;
     private int currentNumber;
+    
 
     private void Start()
     {
         gameUI = FindObjectOfType<GameUIController>();
         cardStack = FindObjectOfType<CardStack>();
         players = new List<Player>();
+        didPlayersSlap = new bool[] {false, false, false, false};
         currentPlayer = 0;
         currentNumber = 0;
 
@@ -41,19 +44,62 @@ public class GameHandler : MonoBehaviour
 
     public void SlapButtonPressed(int playerIndex)
     {
+        if (cardStack.cardsOnTable.Count <= 0) return; // 假如桌上沒有任何牌
+
         gameUI.ShowSlapImage(playerIndex);
 
         if (currentNumber == cardStack.GetCardNumberOnTop())
         {
-            Debug.Log("玩家" + playerIndex + "安全");
+            if (CheckHowManyPlayersSlapped() >= 2)
+            {
+                didPlayersSlap[playerIndex] = true;
+                LoseThisTurn(GetSlowestPlayer());
+            }
+            else
+            {
+                didPlayersSlap[playerIndex] = true;
+            }            
         }
         else
         {
-            Debug.Log("玩家" + playerIndex + "輸了");
-            players[playerIndex].CollectAllCardsOnTable(cardStack); // 輸家收下桌上所有的牌
-            gameUI.UpdateCardButtonText(playerIndex, players[playerIndex].GetCardAmount()); // 更新輸家的卡牌數
+            LoseThisTurn(playerIndex);
+        }
+    }
 
-            currentPlayer = playerIndex; // 從輸家開始下一回合
+    private int CheckHowManyPlayersSlapped()
+    {
+        int count = 0;
+        foreach (bool b in didPlayersSlap)
+        {
+            if (b) count++;
+        }
+
+        return count;
+    }
+
+    private int GetSlowestPlayer()
+    {
+        for (int i = 0; i < didPlayersSlap.Length; i++)
+        {
+            if (didPlayersSlap[i] == false) return i;
+        }
+
+        return -1;
+    }
+
+    private void LoseThisTurn(int playerIndex)
+    {
+        Debug.Log("玩家" + playerIndex + "輸了");
+        players[playerIndex].CollectAllCardsOnTable(cardStack); // 輸家收下桌上所有的牌
+        gameUI.UpdateCardButtonText(playerIndex, players[playerIndex].GetCardAmount()); // 更新輸家的卡牌數
+        gameUI.HideHands(); // 隱藏蓋下的手
+
+        currentNumber = 0; // 重置數字
+        currentPlayer = playerIndex; // 從輸家開始下一回合
+
+        for (int i = 0; i < didPlayersSlap.Length; i++) // 把didPlayersSlap全部設置為false
+        {
+            didPlayersSlap[i] = false;
         }
     }
 }
