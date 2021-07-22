274d08090f1b206e998e3b6ea91f18f64098dbca
sh960440 <54958110+sh960440@users.noreply.github.com>
2021-07-22 05:33:06 -0400

    Main Menu

---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameSettings
+{
+    public static int playerAmount = 2;
+    public static int cardAmount = 52;
+    public static bool hasNumberTracker = true;
+    public static bool isEnglishUI = true;
+}
---
+++ Assets/Scripts/MenuUIController.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuUIController : MonoBehaviour
+{
+    RectTransform playerAmountPanel;
+    RectTransform cardAmountPanel;
+    RectTransform numberTrackerPanel;
+    RectTransform languagePanel;
+    
+    void Awake()
+    {
+        playerAmountPanel = transform.Find("PlayerAmount").GetComponent<RectTransform>();
+        cardAmountPanel = transform.Find("CardAmount").GetComponent<RectTransform>();
+        numberTrackerPanel = transform.Find("NumberTracker").GetComponent<RectTransform>();
+        languagePanel = transform.Find("Language").GetComponent<RectTransform>();
+    }
+
+    public void SetPlayerAmount(int playerAmount)
+    {
+        if (GameSettings.playerAmount == playerAmount) return;
+        GameSettings.playerAmount = playerAmount;
+        SwapButtonTransparency(playerAmountPanel);
+    }
+
+    public void SetCardAmount(int cardAmount)
+    {
+        if (GameSettings.cardAmount == cardAmount) return;
+        GameSettings.cardAmount = cardAmount;
+        SwapButtonTransparency(cardAmountPanel);
+    }
+
+    public void SetNumberTrackerVisiablity(bool isVisiable)
+    {
+        if (GameSettings.hasNumberTracker == isVisiable) return;
+        GameSettings.hasNumberTracker = isVisiable;
+        SwapButtonTransparency(numberTrackerPanel);
+    }
+
+    public void SetLanguage(bool isEnglish)
+    {
+        if (GameSettings.isEnglishUI == isEnglish) return;
+        GameSettings.isEnglishUI = isEnglish;
+        SwapButtonTransparency(languagePanel);
+    }
+
+    private void SwapButtonTransparency(RectTransform panel)
+    {
+        Button[] buttons = panel.GetComponentsInChildren<Button>();
+        foreach (Button button in buttons)
+        {
+            if (button.gameObject.GetComponent<Image>().color.a < 1)
+            {
+                button.gameObject.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+            }
+            else
+            {
+                button.gameObject.GetComponent<Image>().color = new Color(1, 1, 1, 0.4f);
+            }
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
